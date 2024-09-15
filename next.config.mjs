8a15a777228da3686285cfa282ec59d402fe3bc3
jovan-cn <jovan.cn@outlook.com>
2024-09-15 16:55:13 +0800

    chore: try inject in workflow next.yml

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const withNextIntl = createNextIntlPlugin(
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output:  process.env.NODE_ENV !== "production" ? undefined: "export", 
   images: {
     unoptimized: process.env.NODE_ENV !== "production" ? false : true,
   },
