4ccfaca9ad839c912ebc86071c01a951f876054a
jovan-cn <jovan.cn@outlook.com>
2024-09-20 00:41:39 +0800

    feat: add drawer

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,16 +2,27 @@
 import { MotionCalendar } from "@/app/components/calendar/motion";
 import { YearProvider } from "../hooks/useYearContext";
 import { StatisticPanel } from "../components/statistic";
+import { useState } from "react";
+import { Drawer } from "../components/drawer";
 
 export default function Home() {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <YearProvider>
       <div className="relative">
+        <button className={"w-12 h-8 text-center rounded bg-cyan-300 text-white"} onClick={() => {
+          setOpen(!open);
+        }}>
+          More
+        </button>
         <MotionCalendar />
 
-        <aside className="md:inline-block fixed absolute top-20 -right-[40%] ">
+        {/*<aside className="md:inline-block fixed absolute top-20 -right-[40%] ">*/}
+        <Drawer title="More" open={open} onClose={() => setOpen(false)}>
           <StatisticPanel />
-        </aside>
+        </Drawer>
+        {/*</aside>*/}
       </div>
     </YearProvider>
   )
---
+++ app/components/drawer.tsx
@@ -0,0 +1,56 @@
+import clsx from "clsx";
+import { ReactNode } from "react"
+import { MdOutlineClose } from "react-icons/md";
+import { useOutsideClick } from "../hooks/useOutsideClick";
+
+
+interface DrawerProps {
+  title: string,
+  children: ReactNode,
+  open: boolean,
+  onClose: () => void,
+  position?: "top" | "bottom" | "left" | "right",
+  size?: number,
+}
+export const Drawer: React.FC<DrawerProps> = (props) => {
+  const {
+    title,
+    children,
+    open = false,
+    onClose,
+    position = "right",
+    size = 200,
+  } = props;
+
+  const handleClose = () => {
+    onClose();
+  }
+
+  /* ref's click event will conflict with button's in parent */
+  const drawerRef = useOutsideClick(handleClose);
+
+  return (
+    <div ref={drawerRef} className={clsx(
+      'fixed bg-slate-50 dark:bg-slate-800 text-sm',
+      'transition-all ease-in duration-700',
+      ["top", "bottom"].includes(position) ? "left-0" : "top-0",
+    )} style={{
+      [position]: open ? '0' : '-100%',
+      width: ["top", 'bottom'].includes(position) ? '100vw' : size,
+      height: ['left', 'right'].includes(position) ? '100vh' : size,
+    }}>
+      <div className={clsx("flex flex-col gap-2 p-4 justify-center")}>
+        <div className={clsx('flex justify-between items-center')}>
+          <p className="font-bold text-base">{title}</p>
+          <MdOutlineClose className={clsx(
+            'cursor-pointer rounded-xl w-6 h-6 p-1',
+            'hover:bg-slate-100 hover:dark:bg-slate-700'
+          )} onClick={handleClose} />
+        </div>
+
+        {children}
+
+      </div>
+    </div>
+  )
+}
